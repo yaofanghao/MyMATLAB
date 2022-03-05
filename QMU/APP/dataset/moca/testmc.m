b1412aabc615b7a8c78cf794ca383ba863572fd8
yaofanghao <yaofanghao7@163.com>
2022-03-05 10:47:54 +0800

    2022.3.4-not complied

--- QMU/APP/dataset/moca/testmc.m
+++ QMU/APP/dataset/moca/testmc.m
@@ -1,7 +1,7 @@
 %% 输入摩擦数据的格式
 load mc_model.mat;
 
-mc_table = table(200, 270, 90);  %测试数据R=200,压力270,摆角30
+mc_table = table(200, 269, 67);  %测试数据R=200,压力270,摆角30
 mc_table.Properties.VariableNames{'Var1'} = 'VarName1';
 mc_table.Properties.VariableNames{'Var2'} = 'VarName2';
 mc_table.Properties.VariableNames{'Var3'} = 'VarName3';
